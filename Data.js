660e93bb56470aea59a198a276cf1b315377ef60
codex8 <sam.nyx@live.com>
2016-09-11 00:11:14 +0530

    holds questions and answers data

---
+++ Data.js
@@ -0,0 +1,38 @@
+function data(){
+//make questions list
+var CubeData = ([
+
+
+/*Start adding question and answer data*/
+
+//question1 start
+{
+"question": "JavaScript is a client side language",
+"answer"  : "correct",
+"video_for_correct"   : "http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4",
+"video_for_incorrect" : "http://tidewellhospice.org/welcome/wp-content/uploads/2015/03/MVI_7523a.mp4"
+},
+//question1 end
+
+{
+"question": "Apple is blue",
+"answer": "incorrect",
+"video_for_correct"   : "",
+"video_for_incorrect" : ""
+},
+
+{
+"question": "sea is blue",
+"answer": "correct",
+"video_for_correct"   : "",
+"video_for_incorrect" : ""
+},
+
+
+]);
+
+
+
+//return data
+return CubeData;
+}
