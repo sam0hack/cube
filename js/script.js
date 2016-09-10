c367299bd753110ba04b8390f82fda44d1c5fabc
codex8 <sam.nyx@live.com>
2016-09-11 00:12:44 +0530

    fixes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-$(function(){
-	
-	$('.anim').on('click',function(){
-		var anim = $(this).attr('data-anim');
-		$('#cube').removeClass().addClass(anim);
-	});
-
-});
+// $(function(){
+//
+// 	$('.anim').on('click',function(){
+// 		var anim = $(this).attr('data-anim');
+// 		$('#cube').removeClass().addClass(anim);
+// 	});
+//
+// });
